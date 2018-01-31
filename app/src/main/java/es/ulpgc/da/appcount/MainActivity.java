9dd492443554ff011196f50934c985be67f03b5e
Javier Miranda <jmiranda@iuma.ulpgc.es>
2018-01-31 12:42:49 +0000

    (MainActivity): Añadimos tests y conectamos nuestra activity con el  boton y la pantalla.

--- app/src/main/java/es/ulpgc/da/appcount/MainActivity.java
+++ app/src/main/java/es/ulpgc/da/appcount/MainActivity.java
@@ -2,12 +2,52 @@ package es.ulpgc.da.appcount;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private TextView pantalla;
+    private Button boton1;
+    private int contador;
+
+    // Esta clase solamente es necesaria si configuramos el observador
+    // del boton utilizando el estilo tradicional de Java (podemos
+    // borrarla si lo configuramos con una clase anónima)
+    class ButtonListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            Log.d("MainActivity", "boton pulsado");
+            contador++;
+            pantalla.setText("" + contador);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Arrancando mi App");
+
+        pantalla = findViewById(R.id.textView);
+        contador = 0;
+        pantalla.setText("" + contador);
+
+        boton1 = findViewById(R.id.button);
+
+        // Version tradicional
+        // boton1.setOnClickListener(new ButtonListener());
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Log.d("MainActivity", "boton pulsado");
+                contador++;
+                pantalla.setText("" + contador);
+            }
+        });
     }
 }
